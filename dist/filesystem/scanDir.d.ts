ec91904d5ed136fc0d6bb3e89871b6e05618bc5b
Philipp Gröschler <git@groeschler.biz>
2021-10-10 23:00:19 +0200

    Deleted "dist" folder - use a postinstall hook instead

--- dist/filesystem/scanDir.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import * as fs from 'fs';
-export declare type StatsWithPath = fs.Stats & {
-    path: string;
-};
-export declare function scanDir(scanpath: string): Promise<StatsWithPath[]>;
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './config';
-export * from './environment';
-export * from './filesystem/rootPath';
-export * from './filesystem/scanDir';
-export * from './inspect';
-export * from './stringify-clone';
